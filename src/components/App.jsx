c49f95beb5e90c89d34bbcb366c7fc0cb52e1ed7
WiseCraftMedia <wisecraftmedia@gmail.com>
2024-11-02 22:53:58 -0400

    first time

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Carousel from "./Carousel";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
